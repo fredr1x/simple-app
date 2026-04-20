4d22e5fcda798f04e359a92c08867e5c02f00ff1
ydt <230103074@gmail.com>
2026-04-20 14:42:02 +0500

    add form

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-import	os
-from flask import Flask, render_template
+from flask import Flask, request
+
 app = Flask(__name__)
-@app.route("/")
-def	home():
-    return	"<h1>App is running<h1>"
+
+@app.route("/", methods=["GET"])
+def form():
+    return """
+    <h2>User Form</h2>
+    <form method="POST" action="/submit">
+        <input name="firstname" placeholder="First name" required />
+        <br><br>
+        <input name="lastname" placeholder="Last name" required />
+        <br><br>
+        <button type="submit">Submit</button>
+    </form>
+    """
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+
+    payload = {
+        "firstname": firstname,
+        "lastname": lastname
+    }
+
+    return f"""
+    <h2>Submitted Data</h2>
+    <pre>{payload}</pre>
+    <a href="/">Go back</a>
+    """
